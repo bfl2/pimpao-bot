daaac8cd992bb1eccb598700fa89553301010a07
Bruno Leal <bfl2@cin.ufpe.br>
2020-03-05 18:19:34 -0300

    Add greeting routine, plays a sound once per stream session

--- src/bot.js
+++ src/bot.js
@@ -4,10 +4,12 @@ client.connect();
 import quizServer from "./quizServer";
 import Quiz from './quiz.js'
 import { messageToUser } from "./utils";
+import soundController from "./soundController";
 
 const quizPayout = 1000;
 quizServer.mountServer()
 var quizServerStatus = quizServer.getStatus();
+var greetedList = []
 // Commands
 client.on("chat", (channel, user, message, self) => {
   if (self) return; // bot message
@@ -15,10 +17,11 @@ client.on("chat", (channel, user, message, self) => {
 
   var isOwnerCommand = ("#" + user.username == channel);
 
+  var res = greetUser(user.username)
+
   // if message has symbol whats mean command - !
   if (message.indexOf("!") !== -1) {
     var parsedCommand = resolve(channel, user, message);
-    //console.log("Channel:"+channel +" | isOwnerCommand "+isOwnerCommand + " | user:"+ user.username)
     switch(parsedCommand.command)
     {
       case "qstart":
@@ -44,7 +47,7 @@ client.on("chat", (channel, user, message, self) => {
       sendTargetChatMessage(user, message);
       message = `!givepoints ${user.username} ${quizPayout}`;
       sendChatMessage(message);
-      setTimeout(function(){ 
+      setTimeout(function(){
          quizServerStatus = "hidden"
          quizServer.eraseScreen();
       }, 5000);
@@ -59,5 +62,13 @@ function sendTargetChatMessage(user, message) {
 
 function sendChatMessage(message) {
   client.action(client.activeChannel, `${message}`);
+}
+
+function greetUser(username) {
+  if(!greetedList.includes(username)) {
+    var res = soundController.playUserGreeting(username)
+    if(res == 'done')
+      greetedList.push(username)
+  }
 }
 
